9101fccef353b7ffb4716553562590d8e38cebcf
Ajuogaaz <brianlinus1753@gmail.com>
2022-01-12 09:29:57 -0600

    added check for https

--- http_client.py
+++ http_client.py
@@ -2,9 +2,24 @@ import socket
 import sys
 
 
-def http_client():
-    # serverport = 80
-    return 0
+def http_client(hostname, port):
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientSocket.connect((hostname, port))
+
+    request = "GET /basic.html HTTP/1.0\r\nHost:%s\r\n\r\n" % hostname
+    clientSocket.send(request.encode())
+
+    response = clientSocket.recv(4096)
+    return response.decode()
+
+
+def isValidUrl(url):
+    return not url[0:5] == 'https'
+
+
+def error(message):
+    print(message, file=sys.stderr)
+    sys.exit(1)
 
 
 def getIPAddress(hostname):
@@ -14,7 +29,11 @@ def getIPAddress(hostname):
 
 def main():
     hostname = sys.argv[1]
-    print(getIPAddress(hostname))
+    if not isValidUrl(hostname):
+        error("Got https instead of http")
+
+    targetPort = 80
+    print(http_client(hostname, targetPort))
 
 
 main()
